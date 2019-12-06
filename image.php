e12a7bd7ac6a03d0ceec43b4e12fca2bae5d2740
stefanbrakovic <brakovic.stefan@gmail.com>
2019-12-06 10:50:50 +0100

    Update image.php

--- image.php
+++ image.php
@@ -4,7 +4,7 @@ $im = imagecreatetruecolor(200, 200);
 $white = imagecolorallocate($im, 255, 255, 255);
 // Line reserved to trigger the webhook in the lab. 
 $deployment = getenv("COLOR");
-
+/*
 if ( $deployment == 'blue') {
   $color = imagecolorallocate($im, 40, 255, 255);
 } elseif ($deployment == 'green')  {
@@ -12,6 +12,8 @@ if ( $deployment == 'blue') {
 } else {
   $color = imagecolorallocate($im, 70, 0, 255);
 }
+*/
+$color = imagecolorallocate($im, 70,10, 255);
 
 // Draw a filled rectangle
 imagefilledrectangle($im, 0, 0, 199, 199, $color);
